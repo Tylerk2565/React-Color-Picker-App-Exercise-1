c4863015e13d47bca0e4bad29f81a268b2e3323c
Tylerk2565 <ty25566@gmail.com>
2025-02-07 13:47:45 -0500

    setting up files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ColorPicker from './components/ColorPicker';
+
+function App() {
+  const colors = ['red', 'green', 'blue', 'yellow', 'purple', 'orange'];
+  
+  return (
+    <div>
+      <h1>Color Picker Application</h1>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/ColorPicker.jsx
@@ -0,0 +1,10 @@
+
+const ColorPicker = ( { colors }) => {
+  return (
+    <div>
+      <h1>Color Picker Application</h1>
+    </div>
+  );
+}
+
+export default ColorPicker;
